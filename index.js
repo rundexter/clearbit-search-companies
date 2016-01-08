77f7145f98a53e7c053a7a4fc5fdfd995b8bcb0d
Alexander Zaretskii <al.zaretskii@gmail.com>
2016-01-08 17:06:18 +0200

    add DEXTER module

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
+var util = require('./util.js');
+var request = require('request').defaults({
+    baseUrl: 'https://discovery.clearbit.com/'
+});
+
+var pickInputs = {
+        'query': 'query',
+        'sort': 'sort'
+    },
+    pickOutputs = {
+        'id': { keyName: 'results', fields: ['id'] },
+        'name': { keyName: 'results', fields: ['name'] },
+        'legalName': { keyName: 'results', fields: ['legalName'] },
+        'domain': { keyName: 'results', fields: ['domain'] },
+        'tags': { keyName: 'results', fields: ['tags'] },
+        'description': { keyName: 'results', fields: ['description'] },
+        'location': { keyName: 'results', fields: ['location'] },
+        'metrics': { keyName: 'results', fields: ['metrics'] }
+    };
+
 module.exports = {
+
     /**
      * The main entry point for the Dexter module
      *
@@ -6,8 +27,24 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
-        var results = { foo: 'bar' };
-        //Call this.complete with the module's output.  If there's an error, call this.fail(message) instead.
-        this.complete(results);
+        var inputs = util.pickInputs(step, pickInputs),
+            validateErrors = util.checkValidateErrors(inputs, pickInputs),
+            apiKey = dexter.environment('clearbit_api_key'),
+            api = '/v1/companies/search';
+
+        if (!apiKey)
+            return this.fail('A [clearbit_api_key] environment variable is required for this module');
+
+        if (validateErrors)
+            return this.fail(validateErrors);
+
+        request.get({uri: api, qs: inputs, auth: { user: apiKey, pass: '' }, json: true}, function (error, response, body) {
+            if (error)
+                this.fail(error);
+            else if (body && body.error)
+                this.fail(body.error);
+            else
+                this.complete(util.pickOutputs(body, pickOutputs));
+        }.bind(this));
     }
 };
